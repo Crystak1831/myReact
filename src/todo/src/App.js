d7de97e8e43e718a76daf295fb8f9e3e51f78166
Crystal1831 <43097573+Crystak1831@users.noreply.github.com>
2020-11-20 00:21:59 -0500

    "add the add function"

--- src/todo/src/App.js
+++ src/todo/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      newItem: '',
+      list: []
+    }
+  }
+
+  changeHandler(e){
+      this.setState({
+          newItem: e.target.value
+      })
+  }
+
+  addHandler =()=>{
+        const newItem = {
+            id: 1+ Math.random(),
+
+            value: this.state.newItem.slice()
+            //和slice一样是shallow copy
+            // value: [...this.state.newItem]
+        };
+
+        const list = [...this.state.list]
+
+          list.push(newItem);
+
+        this.setState({
+            list,
+            newItem:""
+        })
+  }
+
+  deleteHandler(id){
+      const list = [...this.state.list]
+
+      // list.splice(id,1);
+      // const updateList = list.filter(item =>item.id !== id)
+      // this.setState({
+      //     list: updateList
+      // })
+
+  }
+
+
+
+  render() {
+    return (
+        <div className="App">
+          <h1>To Do</h1>
+          <input  type="text"
+                  onChange = {e=>{this.changeHandler(e)}}
+                  value={this.state.newItem}
+          />
+          <button onClick={this.addHandler}>+</button>
+            <ul>
+                {this.state.list.map(item => {
+                    return (
+                        <li key = {item.id}>
+                            {item.value}
+                            <button onClick={() => this.deleteHandler(item.id)}>delete</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+  }
 }
+
+
 
 export default App;
