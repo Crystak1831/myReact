3b2d31f9822d1ea0e495bfbe343d3f732df3fab3
Crystal1831 <43097573+Crystak1831@users.noreply.github.com>
2020-11-29 23:52:16 -0500

    use the action.js to store the action type

--- src/reduxcounter/src/containers/Counter/Counter.js
+++ src/reduxcounter/src/containers/Counter/Counter.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import CounterControl from "../../components/CounterControl/CounterControl";
 import {connect} from 'react-redux'
+import * as actionTypes from '../../store/actions'
 // import PropTypes from 'prop-types';
 
 class Counter extends Component {
@@ -67,12 +68,12 @@ const mapStateToProps = state => {
 //
 const mapDispatchToProps = dispatch => {
     return{
-        onIncrementCounter: () => dispatch({type:'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type:'DECREMENT'}),
-        onAddCounter: () => dispatch({type:'ADD', value: 5}),
-        onSubCounter: () => dispatch({type:'SUB', value: 5}),
-        onStoreResult:() => dispatch({type:'STORE_RESULT'}),
-        onDeleteResult:(id) => dispatch({type:'DELETE_RESULT', resultElId: id})
+        onIncrementCounter: () => dispatch({type:actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type:actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type:actionTypes.ADD, value: 5}),
+        onSubCounter: () => dispatch({type:actionTypes.SUB, value: 5}),
+        onStoreResult:() => dispatch({type:actionTypes.STORE_RESULT}),
+        onDeleteResult:(id) => dispatch({type:actionTypes.DELETE_RESULT, resultElId: id})
     };
 }
 
--- src/reduxcounter/src/store/reducer.js
+++ src/reduxcounter/src/store/reducer.js
@@ -1,3 +1,4 @@
+import * as actionTypes from './actions'
 
 const initialState = {
     counter:0,
@@ -7,7 +8,7 @@ const initialState = {
 const reducer = (state = initialState, action) =>{
 
     switch(action.type){
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             // return{
                 //copy old state property
             const newState = {...state}
@@ -18,29 +19,29 @@ const reducer = (state = initialState, action) =>{
             return newState
                 // counter: state.counter + 1
             // }
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return{
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.value
             }
-        case 'SUB':
+        case actionTypes.SUB:
             return {
                 ...state,
                 counter: state.counter - action.value
             }
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return{
                 ...state,
                 //use concat() instead of push(), 应为concat返回一个新的array，不会改变原数组，而push会改变原数组
                 //即使有'...state'
                 results: state.results.concat({id: new Date(), value: state.counter})
             }
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             /* method1:splice, if we use splice, we need to copy the array first, splice() mutate the old array
             const id = 2;
             const newArray = [...state.results]
