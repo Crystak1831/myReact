a60364d342a2a9c961a8f568266658724dd7e089
Crystal1831 <43097573+Crystak1831@users.noreply.github.com>
2020-12-19 23:49:34 -0500

    get api data and show on the web page

--- src/apidata/src/App.js
+++ src/apidata/src/App.js
@@ -1,20 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 import React from 'react'
 
-const {useState} = React
+const {useEffect,useState} = React;
+
+const fetchRandom = () => {
+    return axios.get('https://api.randomuser.me/')
+        .then(({data}) => {
+            console.log(data)
+            //null 和 2 可以美化JSON data
+            return JSON.stringify(data, null,2)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 function App() {
     const [counter, setCount] = useState(0)
+    const [randomUserDataJSON, setRandomDataJSON] = useState("")
+
+    useEffect(() => {
+        fetchRandom().then((randomData) =>{
+            setRandomDataJSON(randomData || 'No User Data')
+        })
+
+    }, [])
   return (
     <div className="App">
       <h1>Hello</h1>
         <p>{counter}</p>
-        <button onClick={() => {
-            setCount(counter + 1)
-        }}>+</button>
+        {/*<p>*/}
+            <pre>
+            {randomUserDataJSON}
+            </pre>
+        {/*</p>*/}
+
     </div>
   );
 }
 
-export default App;
+
+
+export default App
